0fa1103f198c16da51da6e6a4fd3bb4f8f161e2f
BEQSONA-cmd <tvildiani2001@gmail.com>
2024-08-10 13:57:37 +0200

    Chxikvia OFF

--- answer_server.py
+++ answer_server.py
@@ -3,13 +3,13 @@ import asyncio
 import websockets
 from json import dumps, loads
 from time import sleep
-from bot import bot_reply
+# from bot import bot_reply
 
 def gethost():
     from socket import gethostname
     if gethostname().endswith('42wolfsburg.de'):
         return 'localhost'
-    return 'YOUR_IP_ADDRESS' # YOUR_IP_ADDRESS should be replaced with the IP address of the machine where the server is running
+    return 'localhost' # YOUR_IP_ADDRESS should be replaced with the IP address of the machine where the server is running
 
 chats = {}
 
@@ -37,9 +37,9 @@ async def handle_message(websocket, path):
                 tasks = [client.send(dumps(msg)) for client in chats[chat_id]]
                 await asyncio.wait([asyncio.create_task(task) for task in tasks])
                 await asyncio.sleep(1)
-                response = bot_reply(*msg.values())
-                for client in chats[chat_id]:
-                    await client.send(dumps(makemsg('ChxikviGPT', response)))
+                # response = bot_reply(*msg.values())
+                # for client in chats[chat_id]:
+                #     await client.send(dumps(makemsg('ChxikviGPT', response)))
             else:
                 chats[chat_id] = set()
     finally:
